4bdc1567d01c18ca79759fafdccba0d15d79d622
deanban <pbp2005@gmail.com>
2019-01-07 20:00:11 -0500

    remote database changed

--- config/keys_dev.js
+++ config/keys_dev.js
@@ -3,6 +3,6 @@ const mlab = require('./mlab_keys')
 module.exports = {
 	mongoURI: `mongodb://${mlab.name}:${
 		mlab.pass
-	}@ds117773.mlab.com:17773/devcon`,
-	secretKey: 'secret'
+	}@ds151354.mlab.com:51354/quoverflow`,
+	secretKey: `${mlab.secret}`
 }
